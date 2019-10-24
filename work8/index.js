ff3e6c62724bf88c66312499fc1bd57c2fa4146d
lstgithub578 <2498410641@qq.com>
2019-10-24 15:26:38 +0800

    wrok8-a

--- work8/index.js
+++ work8/index.js
@@ -11,7 +11,7 @@ function calc(func){
 
 function add(num1,num2){
     return num1+num2
-}
+}    
 function sub(num1,num2){
     return num1-num2
 }
@@ -20,4 +20,4 @@ function mul(num1,num2){
 }
 function div(num1,num2){
     return num1/num2
-}
+}     
