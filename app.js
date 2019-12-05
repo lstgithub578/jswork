f5a17cb853babfeb173b8697262bfffe71c109e0
lstgithub578 <2498410641@qq.com>
2019-12-05 14:40:24 +0800

    work14

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const bodyParser = require("body-parser")
 
 app.use(express.static('.'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:false }));
-app.post('/formBuilder', function (req, res){
+app.use(bodyParser.urlencoded({ extended: false}));
+app.post('/formBuilder',function (req, res) {
     console.log(req.body)
     res.send(req.body)
 })
 
-app.listen(8080, () => console.log('node express 服务器已启动，监听端口:8080'))
+app.listen(8080, () => console.log('node express 服务器已启动, 监听端口:8080'))
 
-const openDefaultBrowser = function (url) {
+const openDenfaultBrowser = function (url) {
     var exec = require('child_process').exec;
     switch (process.platform) {
         case "darwin":
-            exec('open' + url);
+            exec('open ' + url);
             break;
         case "win32":
-            exec('start '+url);
+            exec('start ' + url);
             break;
         default:
-            exec('xdg-open',[url]);
+            exec('xdg-open', [url]);
     }
 }
-openDefaultBrowser('http://localhost:8080')
+openDenfaultBrowser('http://localhost:8080')
